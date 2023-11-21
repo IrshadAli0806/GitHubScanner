3bcaa42849b02a4cb1af4904ee59b480eac847cf
IrshadAli0806 <81609101+IrshadAli0806@users.noreply.github.com>
2023-11-21 17:50:33 +0530

    added test cases
    added test cases

---
+++ __tests__/app.test.ts
@@ -0,0 +1,51 @@
+// __tests__/app.test.ts
+import request from 'supertest';
+import express from 'express';
+import { router } from '../src/routes';
+
+const app = express();
+app.use(express.json());
+app.use('/', router);
+
+describe('GET /repositories', () => {
+  it('should return a list of repositories', async () => {
+    const response = await request(app).get('/repositories');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expect.any(Array));
+  });
+
+  it('should handle errors when fetching repositories', async () => {
+    // Mock the githubRequest function to simulate an error
+    jest.mock('../src/githubApi', () => ({
+      githubRequest: jest.fn().mockRejectedValue(new Error('GitHub API error')),
+    }));
+
+    const response = await request(app).get('/repositories/123');
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('GET /repository/:owner/:repo1/:repo2?', () => {
+  it('should return details of one repository', async () => {
+    const response = await request(app).get('/repository/irshadali0806/repoA');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expect.any(Object));
+  });
+
+  it('should return details of two repositories', async () => {
+    const response = await request(app).get('/repository/irshadali0806/repoA/repoA');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expect.any(Array));
+  });
+
+  it('should handle errors when fetching repository details', async () => {
+    // Mock the getRepositoryDetails function to simulate an error
+    jest.mock('../src/repositoryService', () => ({
+      getRepositoryDetails: jest.fn().mockRejectedValue(new Error('Repository details error')),
+    }));
+
+    const response = await request(app).get('/repository/irshadali0806/repoA1/repoA1');
+    //expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('error', 'Not Found');
+  });
+});
