c578785897277fab00e1945edcb70462f797087c
IrshadAli0806 <81609101+IrshadAli0806@users.noreply.github.com>
2023-11-25 11:47:04 +0530

    change the port
    change the port

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 // src/app.ts
 import express from 'express';
+import cors from 'cors'
 import { router } from './routes';
 
 const app = express();
 const port = process.env.PORT;
 
+
+// Enable CORS for all routes
+app.use(cors());
 // Middleware to parse JSON in requests
 app.use(express.json());
 
